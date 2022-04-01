1bb00acd343b849565c9b96a6c53e3917967c836
Apratim Sadhu <apratimsadhu01@gmail.com>
2022-04-02 00:34:34 +0530

    Update app.py

--- deployment/webapp/app.py
+++ deployment/webapp/app.py
@@ -1,29 +1,25 @@
-import cv2
-from flask import Flask,jsonify,request,send_from_directory,render_template
-from keras_preprocessing import image
-import tensorflow as tf
+from flask import Flask,request,send_from_directory,render_template
+from keras.preprocessing import image
 from keras.models import load_model
 import numpy as np
 import os
 
 app=Flask(__name__)
 
-STATIC_FOLDER='G:/py prog/machine learning-deep learning/covid-19/deployment/webapp/static'
+# STATIC_FOLDER='static'
+STATIC_FOLDER='G:/Code/py prog/machine learning-deep learning/my projects/covid-19 prediction/deployment/webapp/static'
 
 UPLOAD_FOLDER=STATIC_FOLDER+'/uploads/' #path to the folder where we will store the upload before prediction
-MODEL_FOLDER=STATIC_FOLDER+'/models' #path to the folders where we'll store the models
+MODEL_FOLDER=STATIC_FOLDER+'/models/' #path to the folders where we'll store the models
 
 def predict(fullpath):
     data=image.load_img(fullpath,target_size=(100,100,3))
     data=np.expand_dims(data,axis=0)
     data = data.astype('float') / 255
 
-    model=load_model(MODEL_FOLDER+'/model8.h5')
+    model=load_model(MODEL_FOLDER+'/model.h5')
     result=model.predict(data)
-
-    pred_prob=model.predict_proba(data)
-
-    return result,pred_prob
+    return result
 
 @app.route('/')
 def index():
@@ -38,24 +34,24 @@ def upload_file():
         fullname=os.path.join(UPLOAD_FOLDER,file.filename)
         file.save(fullname)
 
-        result,pred_prob=predict(fullname)
+        result=predict(fullname)
         pred=np.argmax(result,axis=1)
-        prob=np.max(pred_prob,axis=1)
-        prob_str=' '.join(map(str, np.round_(prob*100,decimals=2)))
+        prob_str=result
+        # prob_str=' '.join(map(str, np.round_(prob_str*100,decimals=2)))
 
         if pred==0:
             label='Healthy'
-            accuracy=prob_str
+            accuracy=np.round_(np.amax(prob_str)*100,decimals=2)
             return render_template('predict.html',image_file=file.filename,prediction_text='There is a {}% chance that these are {} lungs.'.format(accuracy,label))
 
         elif pred==1:
             label='Viral Pneumonia'
-            accuracy=prob_str
+            accuracy=np.round_(np.amax(prob_str)*100,decimals=2)
             return render_template('predict.html',image_file=file.filename,prediction_text='There is a {}% chance that these lungs have {}.'.format(accuracy,label))
 
         else:
             label='Covid-19'
-            accuracy=prob_str
+            accuracy=np.round_(np.amax(prob_str)*100,decimals=2)
             return render_template('predict.html',image_file=file.filename,prediction_text='There is a {}% chance that these lungs are infected with {}.'.format(accuracy,label))
 
 @app.route('/upload/<filename>')
